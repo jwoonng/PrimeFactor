4b4599dcdcfa4f28875af6aae15db7f6682d0a61
jw0825.lim <iju825@naver.com>
2026-05-28 09:10:15 +0900

    of6

--- prime_factors_test.cpp
+++ prime_factors_test.cpp
@@ -28,3 +28,8 @@ TEST_F(PrimeFixture, of4) {
 	vector<int> expected = { 2, 2 };
 	EXPECT_EQ(expected, prime_factor.of(4));
 }
+
+TEST_F(PrimeFixture, of6) {
+	vector<int> expected = { 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
